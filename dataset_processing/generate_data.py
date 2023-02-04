d8a313ca7cf2fcaae1f6e97d70718828072ffa99
nbouziani <nacime.bouziani@gmail.com>
2023-02-04 04:27:25 +0000

    Use CheckpointFile to save train/test data + cleanup

--- dataset_processing/generate_data.py
+++ dataset_processing/generate_data.py
@@ -1,6 +1,8 @@
+import os
 import argparse
 import numpy as np
 from mpi4py import MPI
+from tqdm.auto import tqdm, trange
 from numpy.random import default_rng
 
 from firedrake import *
@@ -9,12 +11,12 @@ from firedrake import *
 comm = MPI.COMM_WORLD
 
 
-def random_field(V, N=1, m=15, σ=1.4, seed=2023):
+def random_field(V, N=1, m=15, σ=1.4, tqdm=False, seed=2023):
     # Generate 2D random field with m modes
     rng = default_rng(seed)
     x, y = SpatialCoordinate(V.ufl_domain())
     fields = []
-    for _ in range(N):
+    for _ in trange(N, disable=not tqdm):
         r = 0
         for _ in range(m):
             a, b = rng.standard_normal(2)
@@ -25,19 +27,23 @@ def random_field(V, N=1, m=15, σ=1.4, seed=2023):
     return fields
 
 
-def generate_data(V, ntrain, forward='poisson', noise='normal', scale_noise=1., seed=1234):
+def generate_data(V, data_dir, ntrain=50, forward='poisson', noise='normal', scale_noise=1., seed=1234):
     """Generate train/test data for
 
         forward: or a callable
     """
-    ks = random_field(V, ntrain=ntrain+1, seed=seed)
+    print(f"\n Generate random fields")
+    ks = random_field(V, N=ntrain+1, tqdm=True, seed=seed)
+
+    print(f"\n Generate corresponding PDE solutions")
+
     if forward == 'poisson':
         us = []
         v = TestFunction(V)
         x, y = SpatialCoordinate(V.ufl_domain())
         f = Function(V).interpolate(sin(pi * x) * sin(pi * y))
         bcs = [DirichletBC(V, Constant(0.0), "on_boundary")]
-        for k in ks:
+        for k in tqdm(ks):
             u = Function(V)
             F = (inner(exp(k) * grad(u), grad(v)) - inner(f, v)) * dx
             # Solve PDE using LU solver
@@ -48,10 +54,12 @@ def generate_data(V, ntrain, forward='poisson', noise='normal', scale_noise=1., 
     else:
         raise NotImplementedError('Forward problem not implemented. Use "poisson" or provide a callable for your forward problem.')
 
+    print(f"\n Form noisy observations of PDE solutions")
+
     # Add noise to PDE solutions
     if noise == 'normal':
         us_obs = []
-        for u in us:
+        for u in tqdm(us):
             noise = scale_noise * np.random.rand(V.dim())
             u.dat.data[:] += noise
             us_obs.append(u)
@@ -60,14 +68,29 @@ def generate_data(V, ntrain, forward='poisson', noise='normal', scale_noise=1., 
     else:
         raise NotImplementedError('Noise distribution not implemented. Use "normal" or provide a callable for your noise distribution.')
 
-    # Gather data
+    print(f"\n Generated {ntrain} training samples and 1 test sample.")
 
     # Split into train/test
-    #*ks_train, k_test = ks
-    #*us_train, u_test = us
-    #*us_obs_train, u_obs_test = us_obs
+    *ks_train, k_test = ks
+    *us_train, u_test = us
+    *us_obs_train, u_obs_test = us_obs
 
-    #return k_exact, u_exact, u_obs
+    print(f"\n Saving train/test data to {os.path.abspath(data_dir)}.")
+
+    # Save train data
+    with CheckpointFile(os.path.join(data_dir, "train_data.h5"), 'w') as afile:
+        afile.save_mesh(mesh)
+        for i, (k, u, u_obs) in enumerate(zip(ks_train, us_train, us_obs_train)):
+            afile.save_function(k, idx=i, name="k")
+            afile.save_function(u, idx=i, name="u")
+            afile.save_function(u_obs, idx=i, name="u_obs")
+
+    # Save test data
+    with CheckpointFile(os.path.join(data_dir, "test_data.h5"), 'w') as afile:
+        afile.save_mesh(mesh)
+        afile.save_function(k_test, name="k")
+        afile.save_function(u_test, name="u")
+        afile.save_function(u_obs_test, name="u_obs")
 
 
 if __name__ == "__main__":
@@ -81,11 +104,19 @@ if __name__ == "__main__":
     parser.add_argument("--Lx", default=1., type=float, help="Length of the domain")
     parser.add_argument("--Ly", default=1., type=float, help="Width of the domain")
     parser.add_argument("--degree", default=1, type=int, help="Degree of the finite element CG space")
+    parser.add_argument("--resources_dir", default="../data", type=str, help="Resources directory")
+    parser.add_argument("--name_dir", default="poisson_data", type=str, help="Name of the directory to save the data in")
 
     args = parser.parse_args()
 
     # Set up mesh and finite element space
-    mesh = RectangleMesh(args.nx, args.ny, args.Lx, args.Ly)
+    mesh = RectangleMesh(args.nx, args.ny, args.Lx, args.Ly, name="mesh")
     V = FunctionSpace(mesh, "CG", args.degree)
+    # Set up data directory
+    data_dir = os.path.join(args.resources_dir, "datasets", args.name_dir)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
     # Generate data
-    generate_data(V, args.ntrain, args.forward, args.noise, args.scale_noise)
+    generate_data(V, data_dir=data_dir, ntrain=args.ntrain,
+                  forward=args.forward, noise=args.noise,
+                  scale_noise=args.scale_noise)
