59b807629e90f268f9411a99e2bfa42783ae5be4
nbouziani <nacime.bouziani@gmail.com>
2023-02-04 15:14:10 +0000

    Save best-performing model together with training args

--- training/train_heat_conductivity.py
+++ training/train_heat_conductivity.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+import torch
 import torch.optim as optim
 import firedrake as fd
 
@@ -11,7 +12,8 @@ from tqdm.auto import tqdm, trange
 from firedrake import *
 from firedrake_adjoint import *
 
-from dataset_processing.generate_random_conductivity import random_field
+from dataset_processing.generate_data import random_field
+from dataset_processing.load_data import load_dataset
 from models.autoencoder import EncoderDecoder
 from models.cnn import CNN
 from training.utils import TrainingConfig
@@ -20,7 +22,7 @@ from evaluation.evaluate import evaluate
 
 # Retrieve arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--resources_dir", default='', type=str, help="Resources directory")
+parser.add_argument("--resources_dir", default="../data", type=str, help="Resources directory")
 parser.add_argument("--model", default="encoder", type=str, help="one of [encoder, cnn]")
 parser.add_argument("--conductivity", default="circle", type=str, help="one of [circle, random]")
 parser.add_argument("--scale_noise", default=5e-3, type=float, help="Noise scaling")
@@ -31,10 +33,18 @@ parser.add_argument("--learning_rate", default=1e-3, type=float, help="Learning 
 parser.add_argument("--evaluation_metric", default="L2", type=str, help="Evaluation metric: one of [Lp, H1, Hdiv, Hcurl]")
 parser.add_argument("--Lx", default=1., type=float, help="Width of the domain")
 parser.add_argument("--Ly", default=1., type=float, help="Length of the domain")
+parser.add_argument("--name_dir", default="poisson_data", type=str, help="Directory name used to access datasets and save trained models")
 
 args = parser.parse_args()
 config = TrainingConfig(**dict(args._get_kwargs()))
 
+
+# Load dataset
+dataset_dir = os.path.join(config.resources_dir, "datasets", config.name_dir)
+if not os.path.exists(dataset_dir):
+    raise ValueError(f"Dataset directory {os.path.abspath(dataset_dir)} does not exist")
+
+mesh, (k_train, u_train, u_obs_train), (k_test, u_test, u_obs_test) = load_dataset(config)
 
 def solve_poisson(k, f, V, u_exact):
     """Solve Poisson problem"""
@@ -47,47 +57,14 @@ def solve_poisson(k, f, V, u_exact):
     # Assemble Firedrake L2-loss
     return assemble( 0.5 * (u - u_exact) ** 2 * dx)
 
-
-def residual(k, f, V, u_exact):
-    """Assemble Poisson residual"""
-    v = TestFunction(V)
-    F = (inner(exp(k) * grad(u_exact), grad(v)) - inner(f, v)) * dx
-    bcs = [DirichletBC(V, Constant(0.0), "on_boundary")]
-    return assemble(F, bcs=bcs)
-
-
-mesh = RectangleMesh(50, 50, config.Lx, config.Ly)
 V = FunctionSpace(mesh, "CG", 1)
 v = TestFunction(V)
 x, y = SpatialCoordinate(mesh)
 f = Function(V).interpolate(sin(pi * x) * sin(pi * y))
 
-with stop_annotating():
-    if config.conductivity == "circle":
-        k_exact = Function(V).interpolate(conditional((x - Lx / 2)**2 + (y - Ly / 2)**2 < 0.1, 2, 1))
-    elif config.conductivity == "random":
-        k_exact, = random_field(V)
-    u_exact = Function(V)
-    F = (inner(exp(k_exact) * grad(u_exact), grad(v)) - inner(f, v)) * dx
-    bcs = [DirichletBC(V, Constant(0.0), "on_boundary")]
-    # Solve PDE
-    solve(F == 0, u_exact, bcs=bcs)
-    u_obs = Function(V).assign(u_exact)
-    noise = config.scale_noise * np.random.rand(V.dim())
-    u_obs.dat.data[:] += noise
-
-# fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-# collection = tripcolor(k_exact, axes=axes[0], alpha=1, cmap='jet')
-# fig.colorbar(collection);
-# collection = tripcolor(u_exact, axes=axes[1], alpha=1, cmap='jet')
-# fig.colorbar(collection);
-# collection = tripcolor(u_obs, axes=axes[2], alpha=1, cmap='jet')
-# fig.colorbar(collection);
-
-# plt.show()
-
 fd_backend = fd.get_backend()
 
+# Set
 if config.model == 'encoder':
     model = EncoderDecoder(V.dim())
 elif config.model == 'cnn':
@@ -111,7 +88,7 @@ nepochs = config.epochs
 # Generate synthetic dataset
 *train_data, test_data = [fd_backend.to_ml_backend(x) for x in random_field(V, config.ntrain, seed=123)]
 
-
+best_error = 0.
 for epoch_num in trange(nepochs):
     print(f" Epoch num: {epoch_num}")
 
@@ -142,6 +119,23 @@ for epoch_num in trange(nepochs):
     # Evaluation on the test random field
     error = evaluate(model, V, config=config, data=test_data, metric=config.evaluation_metric)
     print(f" Error ({config.evaluation_metric}): {error}")
+
+    # Save best-performing model
+    if error < best_error or epoch_num == 0:
+        best_error = error
+        # Create directory for trained models
+        model_dir = os.path.join(config.resources_dir, "saved_models", config.name_dir)
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        # Save model
+        print(f"Saving model checkpoint to {model_dir}")
+        model_to_save = (
+                model.module if hasattr(model, "module") else model
+            )  # Take care of distributed/parallel training
+        torch.save(model_to_save.state_dict(), os.path.join(model_dir, "model.pt"))
+        # Save training arguments together with the trained model
+        config.to_file(os.path.join(model_dir, "training_args.json"))
+
 
 # Plot
 nn = 3
