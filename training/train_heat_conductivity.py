6a124345f8e0000039a051bddc03401daca7e959
nbouziani <nacime.bouziani@gmail.com>
2023-02-09 09:15:52 +0000

    Clean up

--- training/train_heat_conductivity.py
+++ training/train_heat_conductivity.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 import functools
 
 import torch
@@ -19,7 +20,7 @@ from training.utils import TrainingConfig, get_logger
 from evaluation.evaluate import evaluate
 
 
-logger = get_logger(__name__)
+logger = get_logger("Training")
 
 
 # Retrieve arguments
@@ -32,7 +33,7 @@ parser.add_argument("--learning_rate", default=5e-5, type=float, help="Learning 
 parser.add_argument("--evaluation_metric", default="L2", type=str, help="Evaluation metric: one of [Lp, H1, Hdiv, Hcurl]")
 parser.add_argument("--max_eval_steps", default=5000, type=int, help="Maximum number of evaluation steps")
 parser.add_argument("--dataset", default="poisson", type=str, help="Dataset name: one of [poisson]")
-parser.add_argument("--name_dir", default="data", type=str, help="Directory name to save trained models")
+parser.add_argument("--model_dir", default="model", type=str, help="Directory name to save trained models")
 
 args = parser.parse_args()
 config = TrainingConfig(**dict(args._get_kwargs()))
@@ -114,11 +115,14 @@ H = fd.torch_operator(F)
 set_working_tape(tape)
 
 # Training loop
-for epoch_num in trange(config.epochs):
+for epoch_num in trange(config.epochs, disable=True):
     logger.info(f"Epoch num: {epoch_num}")
 
     model.train()
 
+    total_loss = 0.0
+    total_loss_uk = 0.0
+    total_loss_k = 0.0
     for step_num, batch in tqdm(enumerate(train_data), total=len(train_data)):
 
         model.zero_grad()
@@ -132,32 +136,35 @@ for epoch_num in trange(config.epochs):
 
         # Solve PDE for k_P and assemble the L2-loss: 0.5 * ||u(k) - u_obs||^{2}_{L2}
         loss_uk = G(k, u_obs)
+        total_loss_uk += loss_uk.item()
         # Assemble L2-loss: 0.5 * ||k - k_exact||^{2}_{L2}
         loss_k = H(k, k_exact)
+        total_loss_k += loss_k.item()
 
         # Total loss
         loss = loss_k + config.alpha * loss_uk
+        total_loss += loss.item()
 
         # Backprop and perform Adam optimisation
         loss.backward()
         optimiser.step()
 
-    logger.info(f"Total loss: {loss.item()}")\
-                  \n\t Loss_uk: {loss_uk.item()}  Loss_k: {loss_k.item()}")
+    logger.info(f"Total loss: {total_loss/len(train_data)}\
+                  \n\t Loss_uk: {total_loss_uk/len(train_data)}  Loss_k: {total_loss_k/len(train_data)}")
 
     # Evaluation on the test random field
-    error = evaluate(model, config, test_data, V)
+    error, *_ = evaluate(model, config, test_data, disable_tqdm=True)
     logger.info(f"Error ({config.evaluation_metric}): {error}")
 
-    error_train = evaluate(model, config, train_data, V)
-    logger.info(f"Debug Error ({config.evaluation_metric}): {error_train}")
+    #error_train, *_ = evaluate(model, config, train_data[:50], disable_tqdm=True)
+    #logger.info(f"Debug Error ({config.evaluation_metric}): {error_train}")
 
     # Save best-performing model
     if error < best_error or epoch_num == 0:
         best_error = error
         # Create directory for trained models
-        data_dir = os.path.join(config.resources_dir, "saved_models", config.dataset, config.name_dir)
-        model_dir = os.path.join(data_dir, f"epoch-{epoch_num}-error_{best_error:.5f}")
+        name_dir = f"{config.dataset}-epoch-{epoch_num}-error_{best_error:.5f}"
+        model_dir = os.path.join(config.resources_dir, "saved_models", config.model_dir, name_dir)
         if not os.path.exists(model_dir):
             os.makedirs(model_dir)
         # Save model
