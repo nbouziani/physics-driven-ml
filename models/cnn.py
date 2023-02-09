1dc3b11ca59c9314388db6de29d97184007c1c36
nbouziani <nacime.bouziani@gmail.com>
2023-02-09 09:06:39 +0000

    Refactor CNN model

--- models/cnn.py
+++ models/cnn.py
@@ -1,4 +1,6 @@
-from torch.nn import Module, Sequential, Linear, ReLU, Tanh, MaxPool2d, Conv2d, ConvTranspose2d
+import os
+import torch
+from torch.nn import Module, Sequential, Linear, ReLU, Tanh, MaxPool2d, Conv2d, ConvTranspose2d, BatchNorm2d
 
 
 class CNN(Module):
@@ -9,25 +11,35 @@ class CNN(Module):
         self.n = int(self.dim ** 0.5)
 
         self.linear_in = Linear(self.dim, self.n**2)
+        self.linear_in2 = Linear(self.n**2, self.n**2)
+        self.linear_out2 = Linear(self.n*(self.n - 2)+1, self.n*(self.n - 2)+1) 
         self.linear_out = Linear(self.n*(self.n - 2)+1, self.dim)
 
         self.encoder = Sequential(
-                            Conv2d(1, 64, kernel_size=3,
-                                      padding=1),
+                            Conv2d(1, 32, kernel_size=4,
+                                      padding=2),
+                            BatchNorm2d(32),
                             ReLU(True),
                             MaxPool2d(2, 2),
-                            Conv2d(64, 100, kernel_size=3,
+                            Conv2d(32, 64, kernel_size=4,
                                       padding=1),
+                            BatchNorm2d(64),
+                            ReLU(True),
+                            MaxPool2d(2, 2),
+                            Conv2d(64, 128, kernel_size=3,
+                                      padding=2),
                             ReLU(True))
 
         self.decoder = Sequential(
-                            ConvTranspose2d(100, 64, kernel_size=3,
-                                               stride=1, padding=1),
+                            ConvTranspose2d(128, 64, kernel_size=4,
+                                               padding=2),
+                            BatchNorm2d(64),
                             ReLU(True),
                             ConvTranspose2d(64, 32, kernel_size=3,
                                                stride=2, padding=1),
+                            BatchNorm2d(32),
                             ReLU(True),
-                            ConvTranspose2d(32, 1, kernel_size=4,
+                            ConvTranspose2d(32, 1, kernel_size=4, stride=2,
                                                padding=1),
                             Tanh())
 
@@ -35,12 +47,12 @@ class CNN(Module):
         # x: [batch_size, dim]
         n = self.n
         # Reduce dimensionality to form a grid: x -> [batch_size, :, n, n]
-        x = self.linear_in(x)
+        x = self.linear_in2(F.relu(self.linear_in(x)))
         cnn_input = x.reshape(-1, n, n)[:, None, :]
         # CNN
         encoded = self.encoder(cnn_input)
         decoded = self.decoder(encoded)
         # Recover dimensionality: [batch_size, :, n, n] -> [batch_size, dim]
         y = decoded.squeeze().reshape(-1, n*(n - 2)+1)
-        y = self.linear_out(y)
+        y = self.linear_out(F.relu(self.linear_out2(y)))
         return y
