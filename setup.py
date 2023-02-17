af7cd9cb1f7184a065a036b6330ce429e91c0dfd
nbouziani <nacime.bouziani@gmail.com>
2023-02-17 21:09:28 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="physics-driven-ml",
+    version="1.0",
+    description="Physics-driven machine learning using the Firedrake and PyTorch libraries",
+    author="Nacime Bouziani",
+    author_email="n.bouziani18@imperial.ac.uk",
+    packages=find_packages(),
+)
