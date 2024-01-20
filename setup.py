d8179d0876aeac11f7f9f091114bfd12122eca7f
nbouziani <nacime.bouziani@gmail.com>
2024-01-20 20:04:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+
+# Read README's content
+dir = Path(__file__).parent
+long_description = (dir / "README.md").read_text()
+
 
 setup(
     name="physics-driven-ml",
     version="0.1.0",
     description="Physics-driven machine learning using the Firedrake and PyTorch libraries",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Nacime Bouziani",
     author_email="n.bouziani18@imperial.ac.uk",
     packages=find_packages(),
