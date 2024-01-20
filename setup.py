e9e01e8206f567c76b227a55d99b2293b820305e
nbouziani <nacime.bouziani@gmail.com>
2024-01-20 21:29:35 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,52 @@
+
+import os
+import sys
+import versioneer
+
+import petsc4py
+import numpy as np
+
 from pathlib import Path
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+from Cython.Distutils import build_ext
 
 
 # Read README's content
 dir = Path(__file__).parent
 long_description = (dir / "README.md").read_text()
+
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass['build_ext'] = build_ext
+# List of Cython extensions
+cythonfiles = [("adjacency_dofs", ["petsc"])]
+
+def get_petsc_dir():
+    try:
+        petsc_dir = os.environ["PETSC_DIR"]
+        petsc_arch = os.environ.get("PETSC_ARCH", "")
+    except KeyError:
+        try:
+            petsc_dir = os.path.join(os.environ["VIRTUAL_ENV"], "src", "petsc")
+            petsc_arch = "default"
+        except KeyError:
+            sys.exit("""Error: Firedrake venv not active.""")
+
+    return (petsc_dir, os.path.join(petsc_dir, petsc_arch))
+
+petsc_dirs = get_petsc_dir()
+if os.environ.get("HDF5_DIR"):
+    petsc_dirs = petsc_dirs + (os.environ.get("HDF5_DIR"), )
+include_dirs = [np.get_include(), petsc4py.get_include()]
+include_dirs += ["%s/include" % d for d in petsc_dirs]
+dirs = (sys.prefix, *petsc_dirs)
+link_args = ["-L%s/lib" % d for d in dirs] + ["-Wl,-rpath,%s/lib" % d for d in dirs]
+
+extensions = [Extension("physics_driven_ml.utils.graph.{}".format(ext),
+                        sources=[os.path.join("physics_driven_ml", "utils", "graph", "{}.pyx".format(ext))],
+                        include_dirs=include_dirs,
+                        libraries=libs,
+                        extra_link_args=link_args) for (ext, libs) in cythonfiles]
 
 
 setup(
@@ -17,4 +59,6 @@ setup(
     author_email="n.bouziani18@imperial.ac.uk",
     packages=find_packages(),
     install_requires=["tqdm"],
+    cmdclass=cmdclass,
+    ext_modules=extensions,
 )
