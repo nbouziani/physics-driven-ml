2082368bca95de2745999ab84704fbf30f9bdd97
Nacime Bouziani <48448063+nbouziani@users.noreply.github.com>
2024-01-20 16:53:48 +0000

    Update version (#8)

--- physics_driven_ml/__init__.py
+++ physics_driven_ml/__init__.py
@@ -6,6 +6,11 @@ from physics_driven_ml.training import *                      # noqa: F401
 from physics_driven_ml.evaluation import *                    # noqa: F401
 from physics_driven_ml.utils import ModelConfig, get_logger   # noqa: F401
 
+
+__version__ = "0.1.0"
+__author__ = "Nacime Bouziani"
+
+
 # Add data directory path to environment variables
 package_dir = os.path.abspath(__path__[0])
 os.environ["DATA_DIR"] = os.path.join(os.path.dirname(package_dir), "data")
